83aae6fea42a0063b86c00bd30f55bc2f898126a
rmdm <rmdm@users.noreply.github.com>
2014-07-09 23:16:44 +0400

    Writing test

--- test/nlidb-query.spec.js
+++ test/nlidb-query.spec.js
@@ -2,6 +2,19 @@ var nlidb_query = require('../lib/nlidb-query')(null, null, null);
 
 describe('Set of methods to complete querying db according to some formal representation', function(){
   
+  var funcs;
+  
+  beforeEach(function () {
+    funcs = {
+      f1: function () {
+        return {
+          _transform: function () {},
+          _flush: function () {}
+        };
+      }
+    };
+  });
+  
   it('ueses collectManyLevelResults function to build stream chains', function(){
     
     function onNextLvl (arr, res, buf, i, j) {
@@ -35,6 +48,45 @@ describe('Set of methods to complete querying db according to some formal repres
     expect(res.length).toBe(6);
     var res = nlidb_query.collectManyLevelResults([[1], [2, 3], [3, 4, 5], [6, 7, 8, 9], [10, 11, 12, 13, 14]], onNextLvl);
     expect(res.length).toBe(120);
+    
+  });
+  
+  it('has function nextFunctionalStream to define next functional stream', function () {
+    var rel = {rel: 'A', kvf: []};
+    expect(nlidb_query.nextFunctionalStream(rel, {functions: funcs})).toBe(null);
+    
+    rel.kvf.push({k: 'k1', f: ['f1']});    
+    expect(nlidb_query.nextFunctionalStream(rel, {functions: funcs})).not.toBe(null);    
+  });
+  
+  it('has function buildFunctionalPlumbing to build part of stream chain', function () {
+    var rel = {rel: 'A', kvf: []};
+    var res = nlidb_query.buildFunctionalPlumbing(rel, {functions: funcs});
+    var PassThrough = require('stream').PassThrough;
+    expect(res.head).toBe(res.tail);
+    expect(res.head instanceof PassThrough).toBe(true);
+    expect(res.tail instanceof PassThrough).toBe(true);
+    
+    var rel = {rel: 'A', kvf: [{k: 'k1', f: ['f1']}]};
+    var res = nlidb_query.buildFunctionalPlumbing(rel, {functions: funcs});
+    var PassThrough = require('stream').PassThrough;
+    expect(res.head).toBe(res.tail);
+    expect(res.head instanceof PassThrough).toBe(false);
+    expect(res.tail instanceof PassThrough).toBe(false);
+    
+    var rel = {rel: 'A', kvf: [{k: 'k1', f: ['f1']}, {k: 'k2', f: ['f1']}]};
+    var res = nlidb_query.buildFunctionalPlumbing(rel, {functions: funcs});
+    var PassThrough = require('stream').PassThrough;
+    expect(res.head).not.toBe(res.tail);
+    expect(res.head instanceof PassThrough).toBe(false);
+    expect(res.tail instanceof PassThrough).toBe(false);
+  });
+  
+  it('has function streamRelation to make a stream of current relation', function () {
+    
+  });
+  
+  it('has function buildStreamChains', function () {
     
   });
     
--- test/streams/matchStream.spec.js
+++ test/streams/matchStream.spec.js
@@ -3,70 +3,92 @@ var MatchStream = require('../../lib/streams/matchStream');
 describe('MatchStream', function () {
 
   var fake = {
-    _copyProps: MatchStream.prototype._copyProps,
-    _mergeProps: MatchStream.prototype._mergeProps,
-    _generateOutput: MatchStream.prototype._generateOutput
+    unite: MatchStream.prototype.unite,
+    copy: MatchStream.prototype.copy,
+    filter: MatchStream.prototype.filter,
+    map: MatchStream.prototype.map
   }
   
-  it('has method _copyProps to copy properties of one object to anoter', function () {
-    var obj = {};
-    var e = {a: 45};
-    MatchStream.prototype._copyProps.call(null, obj, e);
-    expect(obj.a).toBe(45);
+  describe('unite method', function () {
+
+    it('merges passed objects', function () {
+      fake.allowed = ['a', 'b'];
+      fake.mapping = {
+        a: 'a',
+        b: 'b'
+      };
+      var res = fake.unite({a: 4}, {b: 5});
+      expect(res.a).toBe(4);
+      expect(res.b).toBe(5);
+      
+      var res = fake.unite({a: 4}, {a: 5});
+      expect(res.a.length).toBe(2);
+      expect(res.a.indexOf(4)).not.toBe(-1);
+      expect(res.a.indexOf(5)).not.toBe(-1);     
+      delete fake.allowed;
+      delete fake.mapping;
+    });
+ 
   });
   
-  it('has method _mergeProps to copy properties of many objects into one object', function () {
-    var obj = {};
-    var objs = [
-      {a: 3, b: 4},
-      {c: 5},
-      {a: 7},
-      {}
-    ];
-    MatchStream.prototype._mergeProps.call(null, obj, objs);
-    expect(obj.b).toBe(4);
-    expect(obj.c).toBe(5);
-    expect(Array.isArray(obj.a)).toBe(true);
-    expect(obj.a.length).toBe(2);
-    expect(obj.a[0]).toBe(3);
-    expect(obj.a[1]).toBe(7);
+  describe('copy method', function () {
+  
+    it('copies only-array-not-object-containing objects', function () {
+      var obj = {
+        x: [1, 2],
+        y: 3
+      };
+      var res = fake.copy(obj);
+      expect(Object.keys(res).length).toBe(2);
+      expect(res.x.length).toBe(2);
+      expect(obj.x.length).toBe(2);
+      obj.x.push(4);
+      expect(res.x.length).toBe(2);
+      expect(obj.x.length).toBe(3);
+      expect(res.y).toBe(3);
+    });
+    
   });
   
-  it('has method _generateOutput to get final representation passed to next stream', function () {    
-    var queue = [
-      {a: 1},
-      {b: 2},
-      {c: 3},
-      {},
-      {d: [4,5]}
-    ];
-    var match = {a: [3,5], b: 1};
+  describe('filter method', function () {
+  
+    it('filters object props according to list of allowed props', function () {    
+      var obj = {
+        x: 1,
+        y: 2,
+        z: 3
+      };
+      var allowed = ['z'];
+      fake.filter(obj, allowed);
+      expect(obj.z).toBe(3);
+      expect(obj.x).not.toBeDefined();
+      expect(obj.y).not.toBeDefined();
+    });
     
-    var res = fake._generateOutput(queue, match);
+  });
+  
+  describe('map method', function () {
     
-    expect(queue.length).toBe(0);    
+    it('renames props of an object', function () {
+      var obj = {
+        a: 1,
+        b: 2,
+        c: 3
+      };
+      var mapping = {
+        a: 'x',
+        b: 'y',
+        c: 'z'
+      }
+      fake.map(obj, mapping);
+      expect(obj.x).toBe(1);
+      expect(obj.y).toBe(2);
+      expect(obj.z).toBe(3);
+      expect(obj.a).not.toBeDefined();
+      expect(obj.b).not.toBeDefined();
+      expect(obj.c).not.toBeDefined();
+    });
     
-    expect(Object.keys(res).length).toBe(4);
-    
-    expect(Array.isArray(res.a)).toBe(true);
-    expect(Array.isArray(res.b)).toBe(true);
-    expect(Array.isArray(res.c)).toBe(false);
-    expect(Array.isArray(res.d)).toBe(true);
-    
-    expect(res.a.length).toBe(3);
-    expect(res.b.length).toBe(2);
-    expect(res.d.length).toBe(2);
-    expect(res.c).toBe(3);
-    
-    expect(res.a[0]).toBe(3);
-    expect(res.a[1]).toBe(5);
-    expect(res.a[2]).toBe(1);
-    
-    expect(res.b[0]).toBe(1);
-    expect(res.b[1]).toBe(2);
-    
-    expect(res.d[0]).toBe(4);
-    expect(res.d[1]).toBe(5);
   });
   
 });
